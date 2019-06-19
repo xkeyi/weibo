551843d89f22a62aa79ad230f6ac7800a8680d0d
vagrant <vagrant@homestead>
2019-06-19 08:07:24 +0000

    用户激活

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Auth;
+use Mail;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -11,7 +12,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['create', 'show', 'store', 'index'],
+            'except' => ['create', 'show', 'store', 'index', 'confirmEmail'],
         ]);
 
         $this->middleware('guest', [
@@ -43,11 +44,12 @@ class UsersController extends Controller
             'password' => \bcrypt($request->password),
         ]);
 
-        Auth::login($user);
+        // Auth::login($user);
+        $this->sendEmailConfirmationTo($user);
 
-        session()->flash('success', '欢迎，您将在这里开启一段的的旅途～');
+        session()->flash('success', '验证邮件已发送到你的注册邮箱了，请检查查收！');
 
-        return redirect()->route('users.show', $user);
+        return redirect('/');
     }
 
     public function edit(User $user)
@@ -114,5 +116,34 @@ class UsersController extends Controller
         $users = User::paginate('10');
 
         return view('users.index', compact('users'));
+    }
+
+    public function sendEmailConfirmationTo(User $user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'foobar@weibo.com';
+        $name = 'Foobar';
+        $to = $user->email;
+        $subject = '感谢注册 Weibo 应用！请确认你的邮箱。';
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
+    }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+
+        session()->flash('success', '恭喜你，激活成功！');
+
+        return redirect()->route('users.show', $user);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('/signup', 'UsersController@create')->name('signup');
+Route::get('/signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
 Route::resource('users', 'UsersController');
 
 Route::get('login', 'SessionsController@create')->name('login');
