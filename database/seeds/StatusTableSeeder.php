8f9b93bc9b675b5687a53e4dc50e412ceabacffa
vagrant <vagrant@homestead>
2019-06-20 09:05:06 +0000

    用户微博列表

---
+++ database/seeds/StatusTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\User;
+use App\Models\Status;
+use Illuminate\Database\Seeder;
+
+class StatusTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user_ids = User::all()->pluck('id');
+
+        $faker = app(Faker\Generator::class);
+
+        $statuses = factory(Status::class)->times(100)->make()->each(function ($status) use ($faker, $user_ids) {
+            $status->user_id = $faker->randomElement($user_ids);
+        });
+
+        Status::insert($statuses->toArray());
+    }
+}
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -10,6 +10,18 @@
         <section class="user_info">
           @include('shared._user_info', ['user' => $user])
         </section>
+
+        <section class="status">
+          @if ($statuses->count() > 0)
+            <ul class="list-unstyled">
+              @foreach ($statuses as $status)
+                @include('statuses._status')
+              @endforeach
+            </ul>
+          @else
+            <p>没有数据</p>
+          @endif
+        </section>
       </div>
     </div>
   </div>
