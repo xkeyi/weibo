7be002b71edda0ff8733bcd556eef98fa4c54353
vagrant <vagrant@homestead>
2019-06-24 08:39:12 +0000

    关注和粉丝列表页面

--- resources/views/shared/_stats.blade.php
+++ resources/views/shared/_stats.blade.php
@@ -1,16 +1,16 @@
-<a href="#">
+<a href="{{ route('users.followings', $user->id) }}">
   <strong id="following" class="stat">
     {{ count($user->followings) }}
   </strong>
   关注
 </a>
-<a href="#">
+<a href="{{ route('users.followers', $user->id) }}">
   <strong id="followers" class="stat">
     {{ count($user->followers) }}
   </strong>
   粉丝
 </a>
-<a href="#">
+<a href="{{ route('users.show', $user->id) }}">
   <strong id="statuses" class="stat">
     <!-- 应该把用户的关注、粉丝、微博数放在缓存中，每次有更新的时候去更新缓存，而不是直接从数据库中读取统计 -->
     {{ $user->statuses()->count() }}
